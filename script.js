c576d8ba7adada4f5faad941c1385c42dedc5edf
P4spid4r <kwamenyamekyethelast@gmail.com>
2024-08-05 13:42:23 +0000

    HELP!

---
+++ script.js
@@ -0,0 +1,6 @@
+//console log text, expression 
+console.log("Hi Kwame")
+console.log(3+7)
+console.log ("This is a story about a child who has litterally no idea whats going on. He needs some majot help. He is very sad")
+   
+   
